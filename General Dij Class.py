0d5782477a179957bb59faa6605b68e3fd89b806
flying_boat <abhilash.munnangi@sjsu.edu>
2019-05-02 17:40:37 -0700

    General Graph File

--- General Dij Class.py
+++ General Dij Class.py
@@ -42,13 +42,6 @@ class Edge:
         self.node1=node1
         self.node2=node2
 
-    def distance(x1, y1, x2, y2):
-        dx = x2 - x1
-        dy = y2 - y1
-        dsquared = dx ** 2 + dy ** 2
-        result = dsquared ** 0.5
-        return result
-
     def either(self):
         return self.node1
 
