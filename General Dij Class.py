8f6fd4d945c0d3b8a8e467fcd5a7192feecd1846
flying_boat <abhilash.munnangi@sjsu.edu>
2019-05-02 23:00:10 -0700

    General Graph File

--- General Dij Class.py
+++ General Dij Class.py
@@ -13,34 +13,42 @@ def distance(x1, y1, x2, y2):
 
 class Graph:
     def __init__(self):
-        self.nodes = list() #Note, using list instead of a set, since Nodes have 3 members, also given data is known to be set
-        self.edges = dict() #nodes are key, example-node 4:edges 4-5,4-6 node 5:5-4,4-6)
+        self.nodes = set() #Note, using list instead of a set, since Nodes have 3 members, also given data is known to be set
+        self.edges = defaultdict(set) #nodes are key, example-node 4:edges 4-5,4-6 node 5:5-4,4-6)
+        self.nodeCordinates= defaultdict(list) #key=node, value = xcord,ycord list
+        # 0:[3,4,5]
+        # 1:[5,6,0]
 
-    def add_node(self, node):
-        assert ( isinstance(node,Node) )
-        #self.nodes.add(node)
-        self.nodes.append(node)
+    def add_node(self, node,xcord,ycord):
+        #self.nodes.append(node)
+        self.nodes.add(node)
+        self.nodeCordinates[node].append(xcord )
+        self.nodeCordinates[node].append(ycord)
 
     def add_edge(self, e):
         assert ( isinstance(e,Edge) )
         v=e.either()
         w=e.other(v)
-        self.edges[v]=e
-        self.edges[w]=e
-        #node object is the key, not node value
-class Node:
-    def __init__(self, value,xcord,ycord):
-        self.val=value
-        self.xcord=xcord
-        self.ycord=ycord
+        self.edges[v].add( w )
+        self.edges[w].add( v )
+        #add distance to weight in future
+
+#class Node:
+   # def __init__(self, value,xcord,ycord):
+        #self.val=value
+       # self.xcord=xcord
+       # self.ycord=ycord
 
 class Edge:
-    def __init__(self, node1,node2 ):
-        assert (isinstance(node1, Node))
-        assert (isinstance(node2, Node))
-        self.weight=distance(node1.xcord,node1.ycord,node2.xcord,node2.ycord)
+    def __init__(self, node1,node2,graph):
+       # assert (isinstance(node1, Node))
+       # assert (isinstance(node2, Node))
+        node1Cord=graph.nodeCordinates[node1]
+        node2Cord=graph.nodeCordinates[node2]
+        self.weight=distance(node1Cord[0],node2Cord[0],node1Cord[1],node2Cord[1])
         self.node1=node1
         self.node2=node2
+        self.NodePair=[node1,node2]
 
     def either(self):
         return self.node1
@@ -64,21 +72,29 @@ class Edge:
 if __name__ == '__main__':
     graph = Graph()
     #adding nodes
-    for i in range(10):
+    for myNode in range(10):
+
         xcord=random.randint(1,20)
         ycord=random.randint(1,20)
-        myNode=Node(i,xcord,ycord)
-        graph.add_node(myNode)
+        #myNode=Node(i,xcord,ycord)
+        graph.add_node(myNode,xcord,ycord)
         #graph.add_node(Node(i,xcord,ycord))
-        print("adding Node",i, "X cord ",xcord ,"Y cord ", ycord)
+        print("adding Node",myNode, "X cord ",xcord ,"Y cord ", ycord)
     #creating like 10 random edges
     for i in range(10):
-        a=graph.nodes[random.randint(1,9)]
-        b=graph.nodes[random.randint(1,9)]
-        myEdge=Edge(a,b)
+        a=random.randint(1,9) #some random node value
+        b=random.randint(1,9)
+        c= random.randint(1,9)
+        myEdge=Edge(a,b,graph)
+        myEdge1 = Edge(a, c,graph)
         graph.add_edge( myEdge )
-        print("adding Edge",a.val,"-",b.val," weight-",myEdge.weight )
-    for x,y in graph.edges:
-        print(x,y)
+        graph.add_edge(myEdge1)
+        print("adding Edge",a,"-",b," weight-",myEdge.weight )
+
+    for x in sorted(graph.edges):
+        print(x,graph.edges[x])
+    #sort hash table
+#
+
 
 
